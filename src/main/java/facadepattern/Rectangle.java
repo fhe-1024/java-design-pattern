8aee70c14a35a06e9d0a1903abbf60eae1bd9a6a
hefei <1042302758@qq.com>
2016-09-03 10:16:13 +0800

    facade pattern.

---
+++ src/main/java/facadepattern/Rectangle.java
@@ -0,0 +1,10 @@
+package facadepattern;
+
+public class Rectangle implements Shape {
+
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Rectangle::draw()");
+	}
+
+}
---
+++ src/main/java/facadepattern/Square.java
@@ -0,0 +1,10 @@
+package facadepattern;
+
+public class Square implements Shape {
+
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Square::draw()");
+	}
+
+}
