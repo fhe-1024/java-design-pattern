42dbde3430db09f9ac43ef94a561542ad5623b32
1042302758@qq.com <1042302758@qq.com>
2016-09-06 18:17:57 +0800

    memento pattern.

---
+++ src/main/java/mamentopattern/Memento.java
@@ -0,0 +1,14 @@
+package mamentopattern;
+
+public class Memento {
+	private String state;
+
+	public Memento(String state) {
+		super();
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+}
